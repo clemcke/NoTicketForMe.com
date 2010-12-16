075e93fb3d5123ab790d446371fd73de8229c46f
Chris Lemcke <clemcke@cpocommerce.com>
2010-12-15 17:49:11 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noticketforme::Application.config.secret_token = 'd9fb3c3bd97220cbd20542f5799eb96a60a783b33ae702ef4ef787d4977b70417afcffd804fbd9c61411a4d80089d5995d8a6803f3dfd79d4ea4ec81f9a7aed4'
