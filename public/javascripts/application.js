0ccf1ff61a009a451d3a46ff26e2a176398c839b
unknown <Chris@.(none)>
2011-01-12 11:45:32 -0800

    completed tab hover effect

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+window.onload = function(){
+$('.nav_tab').hover(function(){
+  $(this).addClass('hover')},function(){
+  $(this).removeClass('hover')});
+}
