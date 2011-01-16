525c1bbc74834867cf977bc3e0a8954b2b4af68a
unknown <Chris@.(none)>
2011-01-16 14:24:27 -0800

    Finished Learn more tabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
       stylesheet_link_tag stylesheet
     end
   end
+
+  def controller_javascript_include_tag
+    javascript = "#{params[:controller]}.js"
+
+    javascript_include_tag javascript if File.exists?(File.join(Rails.public_path, 'javascripts', javascript))
+  end
 end
