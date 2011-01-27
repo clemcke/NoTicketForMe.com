0dc5e4b37d5c38658f3574ac91f490710bb46836
Chris Lemcke <clemcke@YPCMC09056.corp.attinteractive.com>
2011-01-27 10:19:12 -0800

    Fixed typo in payments_controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,7 +1,7 @@
 class PaymentsController < ApplicationController
   protect_from_forgery :except => [:ipn]
   def ipn
-    notify = PaypalNotification.new(params)
+    notify = PayPalNotification.new(params)
 
     #verify with paypal
     if notify.acknowledge
